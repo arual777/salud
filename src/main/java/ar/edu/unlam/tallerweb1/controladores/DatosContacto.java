38dbfda21b46eb2da20ebe181af4a59f913005d9
Julieta Delfino <julietaluciadelfino@hotmail.com>
2021-11-25 19:58:55 -0300

    smtp y modelo de datos

---
+++ src/main/java/ar/edu/unlam/tallerweb1/controladores/DatosContacto.java
@@ -0,0 +1,43 @@
+package ar.edu.unlam.tallerweb1.controladores;
+
+public class DatosContacto {
+    String name;
+    String email;
+    String subject;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    String message;
+
+
+}
